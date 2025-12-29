d8ba23f97f0d2c0a5be26e03d716916437f20cc0
jaykant01 <jaykanty24092004@gmail.com>
2025-12-29 11:28:02 +0530

    [Jaykant] Add .Book Class

---
+++ Constructors-Level1/BookClass.cs
@@ -0,0 +1,35 @@
+﻿namespace Constructors_Level1;
+
+public class BookClass
+{
+    // Attributes
+    public string title { get; set; }
+    public string author { get; set; }
+    public int price { get; set; }
+
+    // Default Constructors
+    public BookClass()
+    {
+        title = "Harry Potter";
+        author = "J.K Rowling";
+        price = 1000;
+    }
+
+    // Parametrized Constructors
+    public BookClass(string Title, string Author, int Price)
+    {
+        title = Title;
+        author = Author;
+        price = Price;
+    }
+
+    // Display
+    public void Display()
+    {
+        Console.WriteLine("Display Book Class");
+        Console.WriteLine("Book Title: " + title);
+        Console.WriteLine("Author Name: " + author);
+        Console.WriteLine("Book Price: " + price + " Rs");
+    }
+
+}
---
+++ Constructors-Level1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection.Metadata;
+
+namespace Constructors_Level1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Problem 1
+        BookClass bk = new BookClass();
+        bk.Display();
+
+        Console.WriteLine();
+
+        BookClass bk1 = new BookClass("Five Points Someone", "Chetan bhagat", 280);
+        bk1.Display();
+
+        Console.ReadKey();
+    }
+}
