af79120f49dfdad3911620bf2a0d8c03661e9080
jaykant01 <jaykanty24092004@gmail.com>
2025-12-29 16:21:39 +0530

    [Jaykant] Add .Book Library System

---
+++ AccessModifiers/BookLibrary.cs
@@ -0,0 +1,47 @@
+﻿namespace AccessModifiers;
+
+public class BookLibrary
+{
+    // Attributes
+    public int ISBN;
+    protected string title;
+    private string author;
+
+    // Parametrized Constructors
+    public BookLibrary(int ISBN, string title, string author)
+    {
+        this.ISBN = ISBN;
+        this.title = title;
+        this.author = author;
+    }
+
+
+    // Get
+    public string GetAuthor()
+    {
+        return author;
+    }
+
+    // Set 
+    public void SetAuthor(string newAuthor)
+    {
+        this.author = newAuthor;
+    }
+
+    // Display
+    public void Display()
+    {
+        Console.WriteLine("Book Details");
+        Console.WriteLine("ISBN: " + ISBN);
+        Console.WriteLine("Book Title: " + title);
+        Console.WriteLine("Book Author: " + author);    
+    }
+
+}
+
+class EBook : BookLibrary
+{
+    public EBook(int ISBN, string title, string author) 
+        : base(ISBN, title, author) { }
+
+}
--- AccessModifiers/Program.cs
+++ AccessModifiers/Program.cs
@@ -4,13 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        UniversityManagement um = new UniversityManagement(101, "Max", 8.5);
-        um.SetCGPA(9.2);
-        um.DisplayInfo();
-        Console.WriteLine();
-        PostgraduateStudent pg = new PostgraduateStudent(102, "Dustin", 8.5);
-        pg.SetCGPA(9.1);
-        pg.DisplayInfo();
+        // Problem 1
+        //UniversityManagement um = new UniversityManagement(101, "Max", 8.5);
+        //um.SetCGPA(9.2);
+        //um.DisplayInfo();
+
+        //Console.WriteLine();
+
+        //PostgraduateStudent pg = new PostgraduateStudent(102, "Dustin", 8.5);
+        //pg.SetCGPA(9.1);
+        //pg.DisplayInfo();
+
+
+        // Problem 2
+        BookLibrary bk = new BookLibrary(4242, "Stranger Things", "Brothers");
+        bk.SetAuthor("Duffer Brothers");
+        bk.Display();
+
+        EBook eb = new EBook(4343, "ST Chapter 5", "Vecna");
+        //eb.SetAuthor("Millie Bobbi Brown");
+        eb.Display();
 
         Console.ReadKey();
         
