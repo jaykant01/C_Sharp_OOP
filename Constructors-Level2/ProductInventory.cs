909a4d40d50aeeb79e68346020984d02cf26c26b
jaykant01 <jaykanty24092004@gmail.com>
2025-12-29 14:35:50 +0530

    [Jaykant] Add .Product Inventory

---
+++ Constructors-Level2/ProductInventory.cs
@@ -0,0 +1,33 @@
+﻿namespace Constructors_Level2;
+
+public class ProductInventory
+{
+    // Instance Variables
+    public string productName;
+    public int price;
+
+    // Class Variable
+    public static int totalProducts;
+
+    // Parametrized Constructors
+    public ProductInventory(string productName, int price)
+    {
+        this.productName = productName;
+        this.price = price;
+        totalProducts++;
+    }
+
+    // Display Product Details
+    public void DisplayProductDetails()
+    {
+        Console.WriteLine("Products Details");
+        Console.WriteLine("Product Name: " + productName);
+        Console.WriteLine("Product Price: " +  price);
+    }
+
+    // Display Total Products
+    public static void DisplayTotalProducts()
+    {
+        Console.WriteLine("Total products created: " + totalProducts);
+    }
+}
---
+++ Constructors-Level2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Constructors_Level2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ProductInventory p = new ProductInventory("IPhone 17", 120000);
+        p.DisplayProductDetails();
+        ProductInventory.DisplayTotalProducts();
+
+        Console.ReadKey();
+    }
+}
