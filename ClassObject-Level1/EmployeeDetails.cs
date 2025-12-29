1fa0e5557151ceeec7cf37b7d425970ba79dc43d
jaykant01 <jaykanty24092004@gmail.com>
2025-12-29 07:28:09 +0530

    [Jaykant] Add .Display Employee Details

---
+++ ClassObject-Level1/EmployeeDetails.cs
@@ -0,0 +1,27 @@
+﻿namespace ClassObject_Level1;
+/* Problem Statement: Write a program to create an Employee class with attributes name, id,
+and salary. Add a method to display the details.
+ */
+public class EmployeeDetails
+{
+    // Fields
+    private string name;
+    private int id;
+    private int salary;
+
+    // Constructor
+    public EmployeeDetails(string name, int id, int salary) { 
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    // Method to Display Employee Details
+    public void DisplayDetails()
+    {
+        Console.WriteLine("Employee Details:");
+        Console.WriteLine("Employee Name: " + name);
+        Console.WriteLine("Employee Id: " + id);
+        Console.WriteLine("Employee Salary: " + salary);
+    }
+}
---
+++ ClassObject-Level1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ClassObject_Level1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Problem 1
+        EmployeeDetails emp = new EmployeeDetails("Jay", 02, 30000);
+        
+        // Display Details
+        emp.DisplayDetails();
+
+        Console.ReadKey();
+    }
+}
