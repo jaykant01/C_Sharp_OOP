270f1db7304efda12d8fa04d347a37a7ee94e4a3
jaykant01 <jaykanty24092004@gmail.com>
2025-12-29 07:39:13 +0530

    [Jaykant] Add .Display Circle Details

---
+++ ClassObject-Level1/AreaOfCircle.cs
@@ -0,0 +1,36 @@
+﻿namespace ClassObject_Level1;
+
+public class AreaOfCircle
+{
+    // Attributes
+    private double radius;
+
+    // Constructor
+    public AreaOfCircle(double radius)
+    {
+        this.radius = radius; 
+    }
+
+    // Method to Calculate Area
+    public double CalculateArea()
+    {
+        return Math.PI * radius * radius;
+    }
+
+    // Method to Calculate circumference
+    public double CalculateCircumference()
+    {
+        return 2 * Math.PI * radius;
+    }
+
+
+    // Display Area Of a Circle
+    public void Display()
+    {
+        Console.WriteLine("Display Circle Details: ");
+        Console.WriteLine("Radius:  " + radius);
+        Console.WriteLine("Area:  " + CalculateArea());
+        Console.WriteLine("Circumference:  " + CalculateCircumference());
+        
+    }
+}
--- ClassObject-Level1/Program.cs
+++ ClassObject-Level1/Program.cs
@@ -5,10 +5,16 @@ class Program
     static void Main(string[] args)
     {
         // Problem 1
-        EmployeeDetails emp = new EmployeeDetails("Jay", 02, 30000);
+        //EmployeeDetails emp = new EmployeeDetails("Jay", 02, 30000);
         
         // Display Details
-        emp.DisplayDetails();
+        //emp.DisplayDetails();
+
+
+        // Problem 2
+        AreaOfCircle c = new AreaOfCircle(8);
+
+        c.Display();
 
         Console.ReadKey();
     }
