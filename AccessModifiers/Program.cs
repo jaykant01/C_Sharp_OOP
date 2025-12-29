9ec5b24621f6ed224443eab79ed945b96e7a28d3
jaykant01 <jaykanty24092004@gmail.com>
2025-12-29 16:08:23 +0530

    [Jaykant] Add .University Management System

--- AccessModifiers/Program.cs
+++ AccessModifiers/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        UniversityManagement um = new UniversityManagement(101, "Max", 8.5);
+        um.SetCGPA(9.2);
+        um.DisplayInfo();
+        Console.WriteLine();
+        PostgraduateStudent pg = new PostgraduateStudent(102, "Dustin", 8.5);
+        pg.SetCGPA(9.1);
+        pg.DisplayInfo();
+
+        Console.ReadKey();
+        
     }
 }
---
+++ AccessModifiers/UniversityManagement.cs
@@ -0,0 +1,61 @@
+﻿namespace AccessModifiers;
+
+public class UniversityManagement
+{
+    // Attributes
+    public int rollNumber;
+    protected string name;
+    private double CGPA;
+
+    // Parametized Constructors
+    public UniversityManagement(int rollNumber, string name, double CGPA)
+    {
+        this.rollNumber = rollNumber;
+        this.name = name;
+        this.CGPA = CGPA;
+    }
+
+    // Getter and setter
+    public double GetCGPA()
+    {
+        return CGPA;
+    }
+
+    public void SetCGPA(double newCGPA)
+    {
+        if (newCGPA >= 0 && newCGPA <= 10)
+        {
+            this.CGPA = newCGPA;
+            Console.WriteLine("CGPA Updated Successfully");
+        }
+        else
+        {
+            Console.WriteLine("Inavalid CGPA value");
+        }
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine("Student Information");
+        Console.WriteLine("Student RollNumber: " + rollNumber);
+        Console.WriteLine("Student Name: " + name);
+        Console.WriteLine("Student CGPA: " + GetCGPA());
+    }
+
+}
+
+class PostgraduateStudent : UniversityManagement
+{
+    public PostgraduateStudent(int rollNumber, string name, double CGPA)
+        : base(rollNumber, name, CGPA)
+    { 
+    }
+
+    public void DisplyInfo()
+    {
+        Console.WriteLine("Student Information");
+        Console.WriteLine("Student RollNumber: " + rollNumber);
+        Console.WriteLine("Student Name: " + name);
+        Console.WriteLine("Student CGPA: " + GetCGPA());
+    }
+}
