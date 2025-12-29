a724522b0a67daf51ccdf2d5993571753e29fb29
jaykant01 <jaykanty24092004@gmail.com>
2025-12-29 14:56:28 +0530

    [Jaykant] Add .Online Course Management

---
+++ Constructors-Level2/OnlineCourse.cs
@@ -0,0 +1,35 @@
+﻿namespace Constructors_Level2;
+
+public class OnlineCourse
+{
+    // Instance Variable
+    public string courseName;
+    public int duration;
+    public int fee;
+
+    // Class Variable
+    public static string instituteName = "Default Name";
+
+    // Parametarized Constructors
+    public OnlineCourse(string courseName, int duration, int fee)
+    {
+        this.courseName = courseName;
+        this.duration = duration;
+        this.fee = fee;
+    }
+
+    // Display Course Details
+    public void DisplayCourseDetails()
+    {
+        Console.WriteLine("Course Details ");
+        Console.WriteLine("Course Name: " + courseName);
+        Console.WriteLine("Course Duration: " + duration);
+        Console.WriteLine("Institute Name: " + instituteName);
+    }
+
+    // Update Institute Name
+    public static void UpdateInstituteName(string newName)
+    {
+        instituteName = newName;
+    }
+}
--- Constructors-Level2/Program.cs
+++ Constructors-Level2/Program.cs
@@ -4,9 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        ProductInventory p = new ProductInventory("IPhone 17", 120000);
-        p.DisplayProductDetails();
-        ProductInventory.DisplayTotalProducts();
+        // Problem 1
+        //ProductInventory p = new ProductInventory("IPhone 17", 120000);
+        //p.DisplayProductDetails();
+        //ProductInventory.DisplayTotalProducts();
+
+
+
+        // Problem 2
+        OnlineCourse o = new OnlineCourse("C#", 3, 0);
+        o.DisplayCourseDetails();
+        Console.WriteLine();
+        OnlineCourse.UpdateInstituteName("GLA");
+        o.DisplayCourseDetails();
+
+
 
         Console.ReadKey();
     }
