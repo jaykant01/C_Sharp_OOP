909753dfaabf521dd634548279aed152c6970c26
jaykant01 <jaykanty24092004@gmail.com>
2025-12-30 11:05:57 +0530

    [Jaykant] Add .Library Management System

---
+++ KeyWords/LibrarySystem.cs
@@ -0,0 +1,45 @@
+﻿namespace KeyWords;
+/* Create a Book class to manage library books with the following features:
+ */
+public class LibrarySystem
+{
+    // Static variable
+    public static string libraryName = "GLA Main Library";
+
+    // Instance Variable
+    public string title;
+    public string author;
+
+    // Readonly Variable
+    public readonly int ISBN;
+
+    // Constructor using this
+    public LibrarySystem(string title, string author, int ISBN)
+    {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+    }
+
+    // static Method to Display Library Name
+    public static void DisplayLibraryName()
+    {    
+        Console.WriteLine("Library Name " + libraryName);  
+    }
+
+    // Display Details
+    public void Display(object obj)
+    {
+        if (obj is LibrarySystem)
+        {
+            Console.WriteLine(" Library Management System ");
+            Console.WriteLine("Title: " + title);
+            Console.WriteLine("Author: " + author);
+            Console.WriteLine("ISBN : " + ISBN);
+        }
+        else
+        {
+            Console.WriteLine("Object is not a Library Mangement System");
+        }
+    }
+}
--- KeyWords/Program.cs
+++ KeyWords/Program.cs
@@ -5,12 +5,18 @@ class Program
     static void Main(string[] args)
     {
         // Problem 1
-        BankSystem b1 = new BankSystem(101, "Nancy", 50000);
-        BankSystem b2 = new BankSystem(102, "Jonathan", 30000);
+        //BankSystem b1 = new BankSystem(101, "Nancy", 50000);
+        //BankSystem b2 = new BankSystem(102, "Jonathan", 30000);
 
-        b1.DisplayDetails(b1);
-        b2.DisplayDetails(b2);
-        BankSystem.GetTotalAccounts();
+        //b1.DisplayDetails(b1);
+        //b2.DisplayDetails(b2);
+        //BankSystem.GetTotalAccounts();
+
+
+        // Problem 2
+        LibrarySystem l1 = new LibrarySystem("Knowledge is Power", "Unknown", 4242);
+        l1.Display(l1);
+        LibrarySystem.DisplayLibraryName();
 
         Console.ReadKey();
         
