7b68613b309b4680e767969e89960ee48cc1ffc0
jaykant01 <jaykanty24092004@gmail.com>
2025-12-31 14:54:34 +0530

    [Jaykant] Add .Bank and Account Holders (Association)

---
+++ ObjectModeling/Bank.cs
@@ -0,0 +1,81 @@
+﻿namespace ObjectModeling;
+/* Description: Model a relationship where a Bank has Customer objects associated with it. A
+Customer can have multiple bank accounts, and each account is linked to a Bank.
+ */
+public class Bank
+{
+    // Attributes 
+    public string bankName { get; }
+    private List<Account> accounts = new List<Account> ();
+
+    // Constructor
+    public Bank(string bankName)
+    {
+        this.bankName = bankName; 
+    }
+
+    // Association Method
+    public Account OpenAccount(Customer customer, int accountNumber)
+    {
+        Account account = new Account(accountNumber, this);
+        accounts.Add(account);
+        customer.AddAccount(account);
+
+        return account;
+    }
+}
+
+public class Account
+{
+    public int accountNumber { get; }
+    public double balance { get; private set; }
+    public Bank bank { get; }
+
+    // Constructor
+    public Account(int accountNumber, Bank bank)
+    {
+        this.accountNumber = accountNumber;
+        this.bank = bank;
+        balance = 0;
+    }
+
+    // Method
+    public void Deposit(double amount)
+    {
+        balance += amount;
+    }
+}
+
+
+public class Customer
+{
+    // Attributes
+    public string name { get; }
+    public List<Account> accounts = new List<Account>();
+
+    // Constructor
+    public Customer(string name)
+    {
+        this.name = name; 
+    }
+
+    // Method
+    public void AddAccount(Account account)
+    {
+        accounts.Add(account);
+    }
+
+    // Method
+    public void ViewBalance()
+    {
+        Console.WriteLine($"Accounts of {name}:");
+        foreach(var account in accounts)
+        {
+            Console.WriteLine(
+                $"Bank: {account.bank.bankName}, " +
+                $"Account No: {account.accountNumber}, " +
+                $"Balance: ₹{account.balance}"
+            );
+        }
+    }
+}
--- ObjectModeling/Program.cs
+++ ObjectModeling/Program.cs
@@ -5,21 +5,34 @@ class Program
     static void Main(string[] args)
     {
         // Aggregation
-        Book book1 = new Book("The Hitchhiker's Guide to the Galaxy", "Douglas Adams");
-        Book book2 = new Book("Dune", "Frank Herbert");
-        Book book3 = new Book("1984", "George Orwell");
+        //Book book1 = new Book("Dune", "Frank Herbert");
+        //Book book2 = new Book("1984", "George Orwell");
+        //Book book3 = new Book("Clean Code", "Robert C. Martin");
 
-        Library cityLibrary = new Library("City Central Library");
-        Library universityLibrary = new Library("University Main Library");
+        //Library cityLibrary = new Library("City Central Library");
+        //Library universityLibrary = new Library("University Main Library");
 
-        cityLibrary.AddBook(book1);
-        cityLibrary.AddBook(book2);
+        //cityLibrary.AddBook(book1);
+        //cityLibrary.AddBook(book2);
+        //// Same book in another library (Aggretion)
+        //universityLibrary.AddBook(book2); 
+        //universityLibrary.AddBook(book3);
 
-        universityLibrary.AddBook(book2);
-        universityLibrary.AddBook(book3);
+        //cityLibrary.DisplayBooks();
+        //universityLibrary.DisplayBooks();
 
-        cityLibrary.DisplayBooks();
-        universityLibrary.DisplayBooks();
+
+        // Association
+        Bank sbi = new Bank("State Bank of India");
+        Customer customer1 = new Customer("Jaykant");
+
+        Account acc1 = sbi.OpenAccount(customer1, 1001);
+        acc1.Deposit(5000);
+
+        Account acc2 = sbi.OpenAccount(customer1, 1002);
+        acc2.Deposit(12000);
+
+        customer1.ViewBalance();
 
         Console.ReadKey();
     }
