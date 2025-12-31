32c2296ea5b9ac2419042205257c504fa157c4c0
jaykant01 <jaykanty24092004@gmail.com>
2025-12-31 12:57:45 +0530

    [Jaykant] Add .Library and Books (Aggregation)

---
+++ ObjectModeling/Book.cs
@@ -0,0 +1,64 @@
+﻿namespace ObjectModeling;
+/* Description: Create a Library class that contains multiple Book objects. Model the
+relationship such that a library can have many books, but a book can exist independently
+(outside of a specific library).
+ */
+public class Book
+{
+    // Attributes & Fields
+    public string title {  get; set; }
+    public string author { get; set; }
+
+    // Constructor
+    public Book(string title, string author)
+    {
+        this.title = title;
+        this.author = author;
+    }
+    
+    // Method
+    public void BookInfo()
+    {
+        Console.WriteLine($"Title of the book is {title} and Author is {author}");
+    }
+
+}
+
+public class Library
+{
+    // Attributes
+    public string name {  get; set; }
+    private List<Book> books = new List<Book>();
+
+    // Constructor
+    public Library(string name)
+    {
+        this.name = name;
+    }
+
+    // Methods
+    public void AddBook(Book book)
+    {
+        if(book != null && !books.Contains(book))
+        {
+            books.Add(book);
+            Console.WriteLine($"Added {book.title} to {name}.");
+        }
+    }
+
+    public void DisplayBooks()
+    {
+        Console.WriteLine($"Books Avialable at {name}");
+        if (books.Any())
+        {
+            foreach (Book book in books)
+            {
+                book.BookInfo();
+            }
+        }
+        else
+        {
+            Console.WriteLine("The Library Currently has no books.");
+        }
+    }
+}
--- ObjectModeling/Program.cs
+++ ObjectModeling/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Aggregation
+        Book book1 = new Book("The Hitchhiker's Guide to the Galaxy", "Douglas Adams");
+        Book book2 = new Book("Dune", "Frank Herbert");
+        Book book3 = new Book("1984", "George Orwell");
+
+        Library cityLibrary = new Library("City Central Library");
+        Library universityLibrary = new Library("University Main Library");
+
+        cityLibrary.AddBook(book1);
+        cityLibrary.AddBook(book2);
+
+        universityLibrary.AddBook(book2);
+        universityLibrary.AddBook(book3);
+
+        cityLibrary.DisplayBooks();
+        universityLibrary.DisplayBooks();
+
+        Console.ReadKey();
     }
 }
