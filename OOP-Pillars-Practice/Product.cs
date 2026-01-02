acaaab05469a300d2a570b5f6e7d50f4f491c5c5
jaykant01 <jaykanty24092004@gmail.com>
2026-01-02 16:07:49 +0530

    [Jaykant] Add .Product Management System

---
+++ OOP-Pillars-Practice/Product.cs
@@ -0,0 +1,105 @@
+﻿namespace OOP_Pillars_Practice;
+
+abstract class Product
+{
+    private int productId;
+    private string name;
+    private double price;
+    
+    public int ProductId
+    {
+        get { return productId; }
+        set { productId = value; }
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public double Price
+    {
+        get { return price; }
+        set { price = value; }
+    }
+
+
+    public Product(int productId, string name, double price)
+    {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+    }
+
+
+    public abstract double CalculateDiscount();
+    
+}
+
+interface ITaxable
+{
+    double CalculateTax();
+
+    string GetTaxDetails();
+}
+
+class Electronics : Product, ITaxable
+{
+    public Electronics(int  productId, string name, double price) : base(productId, name, price)
+    {
+    }
+
+    public override double CalculateDiscount()
+    {
+        return Price * 0.10;
+    }
+
+    public double CalculateTax()
+    {
+        return Price * 0.18;
+    }
+
+    public string GetTaxDetails()
+    {
+        return "18% GST";
+    }
+}
+
+class Clothing : Product, ITaxable
+{
+    public Clothing(int productId, string name, double price) : base(productId, name, price)
+    {
+    }
+
+    public override double CalculateDiscount()
+    {
+        return Price * 0.20; 
+    }
+
+    public double CalculateTax()
+    {
+        return Price * 0.12; 
+    }
+
+    public string GetTaxDetails()
+    {
+        return "12% GST";
+    }
+}
+
+class Groceries : Product
+{
+    public Groceries(int productId, string name, double price) : base(productId, name, price)
+    {
+    }
+
+    public override double CalculateDiscount()
+    {
+        return Price * 0.05;
+    }
+}
+
+
+
+
--- OOP-Pillars-Practice/Program.cs
+++ OOP-Pillars-Practice/Program.cs
@@ -5,23 +5,55 @@ class Program
     static void Main(string[] args)
     {
         // Polymorphism using Employee reference
-        List<Employee> employees = new List<Employee>();
+        //List<Employee> employees = new List<Employee>();
 
-        Employee emp1 = new FullTimeEmployee(101, "Will", 50000);
-        emp1.AssignDepartment("HR");
+        //Employee emp1 = new FullTimeEmployee(101, "Will", 50000);
+        //emp1.AssignDepartment("HR");
 
-        Employee emp2 = new PartTimeEmployee(102, "Henry", 8000, 3, 300);
-        emp2.AssignDepartment("IT");
+        //Employee emp2 = new PartTimeEmployee(102, "Henry", 8000, 3, 300);
+        //emp2.AssignDepartment("IT");
 
-        employees.Add(emp1);
-        employees.Add(emp2);
+        //employees.Add(emp1);
+        //employees.Add(emp2);
 
-        // Polymorphic behavior
-        foreach (Employee emp in employees)
+        //// Polymorphic behavior
+        //foreach (Employee emp in employees)
+        //{
+        //    emp.DisplayDetails();
+        //} 
+
+
+        // Problem 2
+        static void DisplayFinalPrice(List<Product> products)
         {
-            emp.DisplayDetails();
+            foreach (Product p in products)
+            {
+                double tax = 0;
+
+                if (p is ITaxable taxableProduct)
+                {
+                    tax = taxableProduct.CalculateTax();
+                }
+
+                double discount = p.CalculateDiscount();
+                double finalPrice = p.Price + tax - discount;
+
+                Console.WriteLine("Product Name : " + p.Name);
+                Console.WriteLine("Base Price  : " + p.Price);
+                Console.WriteLine("Discount    : " + discount);
+                Console.WriteLine("Tax         : " + tax);
+                Console.WriteLine("Final Price : " + finalPrice);
+                Console.WriteLine();
+            }
         }
 
+        List<Product> products = new List<Product>();
+
+        products.Add(new Electronics(1, "Laptop", 50000));
+        products.Add(new Clothing(2, "Shirt", 2000));
+        products.Add(new Groceries(3, "Rice", 1000));
+
+        DisplayFinalPrice(products);
         Console.ReadKey();
     }
 }
