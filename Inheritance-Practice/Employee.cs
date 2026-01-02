a4e0f6b79c6252ed46ed1d0deb78ead21090b690
jaykant01 <jaykanty24092004@gmail.com>
2026-01-02 11:17:58 +0530

    [Jaykant] Add .Employee Management System

---
+++ Inheritance-Practice/Employee.cs
@@ -0,0 +1,74 @@
+﻿namespace Inheritance_Practice;
+
+public class Employee
+{
+    public string Name;
+    public int Id;
+    public double Salary;
+
+    public Employee(string name, int id, double salary)
+    {
+        Name = name;
+        Id = id;
+        Salary = salary;
+    }
+
+    public virtual void DisplayDetails()
+    {
+        Console.WriteLine("Employee Details:");
+        Console.WriteLine($"Name   : {Name}");
+        Console.WriteLine($"ID     : {Id}");
+        Console.WriteLine($"Salary : {Salary}");
+    }
+}
+
+class Manager : Employee
+{
+    public int TeamSize;
+
+    public Manager(string name, int id, double salary, int teamSie) : base(name, id, salary)
+    {
+        TeamSize = teamSie;
+    }
+
+    public override void DisplayDetails()
+    {
+        base.DisplayDetails();
+        Console.WriteLine($"Team Size : {TeamSize}");
+    }
+}
+
+class Developer : Employee
+{
+    public string ProgrammingLanguage;
+
+    public Developer(string name, int id, double salary, string programmingLanguage)
+        : base(name, id, salary)
+    {
+        ProgrammingLanguage = programmingLanguage;
+    }
+
+    public override void DisplayDetails()
+    {
+        base.DisplayDetails();
+        Console.WriteLine($"Programming Language : {ProgrammingLanguage}");
+    }
+}
+
+class Intern : Employee
+{
+    public string InternshipDuration;
+
+    public Intern(string name, int id, double salary, string internshipDuration)
+        : base(name, id, salary)
+    {
+        InternshipDuration = internshipDuration;
+    }
+
+    public override void DisplayDetails()
+    {
+        base.DisplayDetails();
+        Console.WriteLine($"Internship Duration : {InternshipDuration}");
+    }
+}
+
--- Inheritance-Practice/Program.cs
+++ Inheritance-Practice/Program.cs
@@ -7,15 +7,27 @@ class Program
     static void Main(string[] args)
     {
         // Single Inheritance
-        Animal animal = new Animal();
-        animal.MakeSound();
-        Dog dog = new Dog();
-        dog.MakeSound();
-        Cat cat = new Cat();
-        cat.MakeSound();
-        Bird bird = new Bird();
-        bird.MakeSound();
+        //Animal animal = new Animal();
+        //animal.MakeSound();
+        //Dog dog = new Dog();
+        //dog.MakeSound();
+        //Cat cat = new Cat();
+        //cat.MakeSound();
+        //Bird bird = new Bird();
+        //bird.MakeSound();
 
+
+        Employee manager = new Manager("Alice", 101, 80000, 10);
+        manager.DisplayDetails();
+        Console.WriteLine();
+
+        Employee developer = new Developer("Bob", 102, 60000, "C#");
+        developer.DisplayDetails();
+        Console.WriteLine();
+
+        Employee intern = new Intern("Charlie", 103, 15000, "6 Months");
+        developer.DisplayDetails();
+        Console.WriteLine();
 
         Console.ReadKey();
     }
