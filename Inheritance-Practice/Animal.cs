3e5110a4a4820984c9d873f040576f9407c5d1d1
jaykant01 <jaykanty24092004@gmail.com>
2026-01-02 11:03:09 +0530

    [Jaykant] Add .Animal Hierarchy

---
+++ Inheritance-Practice/Animal.cs
@@ -0,0 +1,46 @@
+﻿namespace Inheritance_Practice;
+/* 1. Animal Hierarchy
+Description:
+Create a hierarchy where Animal is the superclass, and Dog, Cat, and Bird are subclasses.
+Each subclass has a unique behavior.
+ */
+public class Animal
+{
+    public string Name;
+    public int Age;
+
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Animal makes a Sound");
+    }
+
+}
+
+public class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Dog barks");
+    }
+}
+
+public class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Cat meows");
+    }
+}
+
+public class Bird : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bird chirps");
+    }
+}
+
+
+
+
+
--- Inheritance-Practice/Program.cs
+++ Inheritance-Practice/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace Inheritance_Practice;
+﻿using System.Data.Common;
+
+namespace Inheritance_Practice;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Single Inheritance
+        Animal animal = new Animal();
+        animal.MakeSound();
+        Dog dog = new Dog();
+        dog.MakeSound();
+        Cat cat = new Cat();
+        cat.MakeSound();
+        Bird bird = new Bird();
+        bird.MakeSound();
+
+
+        Console.ReadKey();
     }
 }
