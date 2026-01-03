0d52366546bc9ab2c0b1af75dfeba706ab3b11e6
jaykant01 <jaykanty24092004@gmail.com>
2026-01-03 12:35:33 +0530

    [Jaykant] Add .OOP Review

---
+++ Review/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Review;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        static void CreateStudent()
+        {
+            Student s1 = new Student(40, 55, "Arjun", 1201);
+            s1.DisplayInfo();
+            s1.GradeCalculation();
+        }
+
+        // Object Call
+        CreateStudent();   
+
+        GC.Collect();     
+
+        Console.ReadKey();
+    }
+}
